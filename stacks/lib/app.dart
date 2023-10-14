6011255d470382940e6d6f0201b282ba05098b4e
userb3rt <userb3rt@github.com>
2023-10-14 15:34:40 +0200

    .

--- stacks/lib/app.dart
+++ stacks/lib/app.dart
@@ -1,32 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'layout_desktop.dart';
 
-// Main application widget
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
-
   @override
   AppState createState() => AppState();
 }
 
-// Main application state
 class AppState extends State<App> {
-  // Define the layout to use depending on the screen width
-  Widget _setLayout(BuildContext context) {
-    // Set different layouts depending on the screen width
-    double width = MediaQuery.of(context).size.width;
-    return const LayoutDesktop(title: "App Desktop Title");
-
-  }
-
-  // Definir el contingut del widget 'App'
+// Definir el contingut del widget 'App'
   @override
   Widget build(BuildContext context) {
-    // Farem servir la base 'Cupertino'
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: _setLayout(context),
+      home: const LayoutDesktop(),
     );
   }
 }
